2c61677c191805c26c3c2dd2d4794d7542c2986e
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-09-11 13:12:22 +0200

    feat: add prettier-plugin-tailwindcss

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,3 +1,5 @@
 export function App(): JSX.Element {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <h1 className="text-center text-3xl font-bold underline">Hello world!</h1>
+  )
 }
