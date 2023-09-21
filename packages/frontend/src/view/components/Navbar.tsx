3b61775fc8586ec807c1ec4832def7a940bbfb6f
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-09-21 12:41:49 +0200

    Fix header button stretching

--- packages/frontend/src/view/components/Navbar.tsx
+++ packages/frontend/src/view/components/Navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar(): JSX.Element {
   return (
     <nav className="bg-black px-8 py-4">
       <a href="/">
-        <h1 className="flex items-center  gap-5">
+        <h1 className="inline-flex items-center gap-5">
           <LayerZeroLogo className="h-7" />
           <CollabLogo className="h-2.5" />
           <L2BeatLogo className="h-7" />
