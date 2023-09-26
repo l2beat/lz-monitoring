169193c47bdebe2afb8bac7bbcd6be91cae13636
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-09-26 16:49:35 +0200

    feat: add new backend endpoints

--- packages/backend/src/api/routes/discovery.ts
+++ packages/backend/src/api/routes/discovery.ts
@@ -1,14 +1,20 @@
 import Router from '@koa/router'
+import { ChainId, stringAs } from '@lz/libs'
+import { z } from 'zod'
 
 import { DiscoveryController } from '../controllers/discovery/DiscoveryController'
+import { withTypedContext } from './typedContext'
 
 export function createDiscoveryRouter(
   discoveryController: DiscoveryController,
 ): Router {
   const router = new Router()
 
+  /**
+   * @deprecated
+   */
   router.get('/discovery/raw', async (ctx): Promise<void> => {
-    const data = await discoveryController.getRawDiscovery()
+    const data = await discoveryController.getRawDiscovery(ChainId.ETHEREUM)
 
     if (!data) {
       ctx.status = 404
@@ -18,8 +24,11 @@ export function createDiscoveryRouter(
     ctx.body = data
   })
 
+  /**
+   * @deprecated
+   */
   router.get('/discovery', async (ctx): Promise<void> => {
-    const data = await discoveryController.getDiscovery()
+    const data = await discoveryController.getDiscovery(ChainId.ETHEREUM)
 
     if (!data) {
       ctx.status = 404
@@ -28,6 +37,44 @@ export function createDiscoveryRouter(
 
     ctx.body = data
   })
+
+  router.get(
+    '/discovery/:chainId',
+    withTypedContext(
+      z.object({
+        params: z.object({ chainId: stringAs((s) => ChainId(+s)) }),
+      }),
+      async (ctx): Promise<void> => {
+        const data = await discoveryController.getDiscovery(ctx.params.chainId)
+
+        if (!data) {
+          ctx.status = 404
+          return
+        }
+
+        ctx.body = data
+      },
+    ),
+  )
+
+  router.get(
+    '/discovery/raw/:chainId',
+    withTypedContext(
+      z.object({
+        params: z.object({ chainId: stringAs((s) => ChainId(+s)) }),
+      }),
+      async (ctx): Promise<void> => {
+        const data = await discoveryController.getDiscovery(ctx.params.chainId)
+
+        if (!data) {
+          ctx.status = 404
+          return
+        }
+
+        ctx.body = data
+      },
+    ),
+  )
 
   return router
 }
