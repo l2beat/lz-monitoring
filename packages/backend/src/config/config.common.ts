96d20afa812cd627675d5212343ce1325e1c5a87
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-09-28 20:54:34 +0200

    feat: further templates

--- packages/backend/src/config/config.common.ts
+++ packages/backend/src/config/config.common.ts
@@ -1,106 +1,139 @@
 import { Env } from '@l2beat/backend-tools'
+import { DiscoveryConfig } from '@l2beat/discovery'
 // eslint-disable-next-line import/no-internal-modules
 import { UnixTime } from '@l2beat/discovery/dist/utils/UnixTime'
 
-import { Config } from './Config'
+import { Config, EthereumLikeDiscoveryConfig } from './Config'
 import { arbitrumDiscoveryConfig } from './discovery/arbitrum'
 import { baseDiscoveryConfig } from './discovery/base'
+import { bscDiscoveryConfig } from './discovery/bsc'
 import { ethereumDiscoveryConfig } from './discovery/ethereum'
 import { optimismDiscoveryConfig } from './discovery/optimism'
 import { polygonPosDiscoveryConfig } from './discovery/polygon-pos'
 import { polygonZkEvmDiscoveryConfig } from './discovery/polygon-zkevm'
 
 export function getCommonDiscoveryConfig(env: Env): Config['discovery'] {
+  const createConfig = configFromTemplate(env)
+
   return {
     clock: {
       tickIntervalMs: env.integer('CLOCK_TICK_INTERVAL_MS', 10 * 1000),
     },
     modules: {
-      ethereum: env.boolean('ETHEREUM_DISCOVERY_ENABLED', true) && {
-        // This is an arbitrary number so we do not start too far in the past.
-        startBlock: env.integer('ETHEREUM_START_BLOCK', 18127698),
-        rpcUrl: env.string('ETHEREUM_RPC_URL'),
+      ethereum: createConfig({
+        chainNamePrefix: 'ETHEREUM',
+        startBlock: 18127698,
+        blockExplorerPrefix: 'ETHERSCAN',
         blockExplorerApiUrl: 'https://api.etherscan.io/api',
-        blockExplorerApiKey: env.string('ETHERSCAN_API_KEY'),
-        blockExplorerMinTimestamp: new UnixTime(
-          env.integer('ETHERSCAN_MIN_TIMESTAMP', 0),
-        ),
-        discovery: ethereumDiscoveryConfig,
-      },
-      arbitrum: env.boolean('ARBITRUM_DISCOVERY_ENABLED', true) && {
-        startBlock: env.integer('ARBITRUM_START_BLOCK', 133212747),
-        rpcUrl: env.string('ARBITRUM_RPC_URL'),
+        blockExplorerMinTimestamp: new Date(0),
+        discoveryConfig: ethereumDiscoveryConfig,
+      }),
+      arbitrum: createConfig({
+        chainNamePrefix: 'ARBITRUM',
+        startBlock: 133212747,
+        blockExplorerPrefix: 'ARBISCAN',
         blockExplorerApiUrl: 'https://api.arbiscan.io/api',
-        blockExplorerApiKey: env.string('ARBISCAN_API_KEY'),
-        blockExplorerMinTimestamp: new UnixTime(
-          env.integer(
-            'ARBISCAN_MIN_TIMESTAMP',
-            // ~ Timestamp of block number 0 on Arbitrum
-            new Date('2021-05-28T22:15:00Z').getTime() / 1000,
-          ),
-        ),
-        discovery: arbitrumDiscoveryConfig,
-      },
-      optimism: env.boolean('OPTIMISM_DISCOVERY_ENABLED', true) && {
-        startBlock: env.integer('OPTIMISM_START_BLOCK', 109983712),
-        rpcUrl: env.string('OPTIMISM_RPC_URL'),
+        blockExplorerMinTimestamp: new Date('2021-05-28T22:15:00Z'),
+        discoveryConfig: arbitrumDiscoveryConfig,
+      }),
+      optimism: createConfig({
+        chainNamePrefix: 'OPTIMISM',
+        startBlock: 109983712,
+        blockExplorerPrefix: 'OPTIMISTIC_ETHERSCAN',
         blockExplorerApiUrl: 'https://api-optimistic.etherscan.io/api',
-        blockExplorerApiKey: env.string('OPTIMISTIC_ETHERSCAN_API_KEY'),
-        blockExplorerMinTimestamp: new UnixTime(
-          env.integer(
-            'OPTIMISTIC_ETHERSCAN_MIN_TIMESTAMP',
-            // ~ Timestamp of block number 0 on Optimism
-            new Date('2021-01-14T15:52:00Z').getTime() / 1000,
-          ),
-        ),
-        discovery: optimismDiscoveryConfig,
-      },
-
-      'polygon-pos': env.boolean('POLYGON_POS_DISCOVERY_ENABLED', false) && {
-        startBlock: env.integer('POLYGON_POS_START_BLOCK', 48049992),
-        rpcUrl: env.string('POLYGON_POS_RPC_URL'),
+        blockExplorerMinTimestamp: new Date('2021-01-14T15:52:00Z'),
+        discoveryConfig: optimismDiscoveryConfig,
+      }),
+      'polygon-pos': createConfig({
+        chainNamePrefix: 'POLYGON_POS',
+        startBlock: 48049992,
+        blockExplorerPrefix: 'POLYGONSCAN_POS',
         blockExplorerApiUrl: 'https://api.polygonscan.com/api',
-        blockExplorerApiKey: env.string('POLYGONSCAN_POS_API_KEY'),
-        // ~ Timestamp of block number 0 on Polygon-PoS
-        blockExplorerMinTimestamp: new UnixTime(
-          env.integer(
-            'POLYGONSCAN_POS_MIN_TIMESTAMP',
-            new Date('2020-05-30T07:48:00Z').getTime() / 1000,
-          ),
-        ),
-        discovery: polygonPosDiscoveryConfig,
-      },
-      base: env.boolean('BASE_DISCOVERY_ENABLED', false) && {
-        startBlock: env.integer('BASE_START_BLOCK', 4526481),
-        rpcUrl: env.string('BASE_RPC_URL'),
-        blockExplorerApiUrl: 'https://api.basescan.org/api',
-        blockExplorerApiKey: env.string('BASESCAN_API_KEY'),
-        // ~ Timestamp of block number 0 on Base
-        blockExplorerMinTimestamp: new UnixTime(
-          env.integer(
-            'BASESCAN_MIN_TIMESTAMP',
-            new Date('2023-06-15T12:36:00Z').getTime() / 1000,
-          ),
-        ),
-        discovery: baseDiscoveryConfig,
-      },
-      'polygon-zkevm': env.boolean(
-        'POLYGON_ZKEVM_DISCOVERY_ENABLED',
-        false,
-      ) && {
-        startBlock: env.integer('POLYGON_ZKEVM_START_BLOCK', 5793888),
-        rpcUrl: env.string('POLYGON_ZKEVM_RPC_URL'),
+        blockExplorerMinTimestamp: new Date('2020-05-30T07:48:00Z'),
+        discoveryConfig: polygonPosDiscoveryConfig,
+      }),
+      'polygon-zkevm': createConfig({
+        chainNamePrefix: 'POLYGON_ZKEVM',
+        startBlock: 5793888,
+        blockExplorerPrefix: 'POLYGONSCAN_ZKEVM',
         blockExplorerApiUrl: 'https://api-zkevm.polygonscan.com/api',
-        blockExplorerApiKey: env.string('POLYGONSCAN_ZKEVM_API_KEY'),
-        // ~ Timestamp of block number 0 on Polygon-ZK-EVM
+        blockExplorerMinTimestamp: new Date('2023-06-15T12:36:00Z'),
+        discoveryConfig: polygonZkEvmDiscoveryConfig,
+      }),
+      base: createConfig({
+        chainNamePrefix: 'BASE',
+        startBlock: 4526481,
+        blockExplorerPrefix: 'BASESCAN',
+        blockExplorerApiUrl: 'https://api.basescan.org/api',
+        blockExplorerMinTimestamp: new Date('2023-06-15T12:36:00Z'),
+        discoveryConfig: baseDiscoveryConfig,
+      }),
+
+      bsc: createConfig({
+        chainNamePrefix: 'BSC',
+        startBlock: 20783073,
+        blockExplorerPrefix: 'BSCSCAN',
+        blockExplorerApiUrl: 'https://api.bscscan.com/api',
+        blockExplorerMinTimestamp: new Date('2021-09-01T00:00:00Z'),
+        discoveryConfig: bscDiscoveryConfig,
+      }),
+    },
+  }
+}
+
+function configFromTemplate(env: Env) {
+  return function ({
+    chainNamePrefix,
+    startBlock,
+    blockExplorerPrefix,
+    blockExplorerApiUrl,
+    blockExplorerMinTimestamp,
+    discoveryConfig,
+  }: {
+    /**
+     * The prefix of the environment variables that configure the chain.
+     */
+    chainNamePrefix: string
+
+    /**
+     * The prefix of the environment variables that configure the block explorer.
+     */
+    blockExplorerPrefix: string
+
+    /**
+     * The URL of the block explorer API.
+     */
+    blockExplorerApiUrl: string
+
+    /**
+     * The discovery configuration.
+     */
+    discoveryConfig: DiscoveryConfig
+
+    /**
+     * The minimum timestamp block explorer client can query
+     */
+    blockExplorerMinTimestamp: Date
+
+    /**
+     * The block number from which to start indexing.
+     */
+    startBlock: number
+  }): false | EthereumLikeDiscoveryConfig {
+    return (
+      env.boolean(`${chainNamePrefix}_DISCOVERY_ENABLED`, false) && {
+        startBlock: env.integer(`${chainNamePrefix}_START_BLOCK`, startBlock),
+        rpcUrl: env.string(`${chainNamePrefix}_RPC_URL`),
+        blockExplorerApiUrl,
+        blockExplorerApiKey: env.string(`${blockExplorerPrefix}_API_KEY`),
         blockExplorerMinTimestamp: new UnixTime(
           env.integer(
-            'POLYGONSCAN_ZKEVM_MIN_TIMESTAMP',
-            new Date('2023-06-15T12:36:00Z').getTime() / 1000,
+            `${blockExplorerPrefix}_MIN_TIMESTAMP`,
+            blockExplorerMinTimestamp.getTime() / 1000,
           ),
         ),
-        discovery: polygonZkEvmDiscoveryConfig,
-      },
-    },
+        discovery: discoveryConfig,
+      }
+    )
   }
 }
