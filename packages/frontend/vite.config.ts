98e5d857e2964b95176197ba3c115ec092664cbc
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-10-03 08:39:45 +0200

    chore: bring back preserve symlinks

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: { preserveSymlinks: true },
   optimizeDeps: {
     include: ['@lz/libs'],
   },
