bb5f80cd361c23d24d810ce97395ba0827b2f689
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-10-03 08:21:39 +0200

    chore: improve vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: { preserveSymlinks: true },
+  optimizeDeps: {
+    include: ['@lz/libs'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@lz\/libs/, /node_modules/],
+    },
+  },
 })
