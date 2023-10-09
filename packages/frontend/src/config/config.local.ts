af8ce4f731d2db225ac7d3162864b7fc48d10d68
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-10-09 15:02:20 +0200

    feat: add Linea

--- packages/frontend/src/config/config.local.ts
+++ packages/frontend/src/config/config.local.ts
@@ -14,6 +14,7 @@ export function getLocalConfig(): Config {
       ChainId.BASE,
       ChainId.AVALANCHE,
       ChainId.BSC,
+      ChainId.LINEA,
     ],
   }
 }
--- packages/frontend/src/config/config.staging.ts
+++ packages/frontend/src/config/config.staging.ts
@@ -14,6 +14,7 @@ export function getStagingConfig(): Config {
       ChainId.BASE,
       ChainId.AVALANCHE,
       ChainId.BSC,
+      ChainId.LINEA,
     ],
   }
 }
