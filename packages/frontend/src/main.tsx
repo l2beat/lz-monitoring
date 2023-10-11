c65d612d6cd639bcbc8ecb1488fb7ea5a2029197
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-10-11 16:54:22 +0200

    feat: remove global skeleton theme provider

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -3,7 +3,6 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SkeletonTheme } from 'react-loading-skeleton'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { Main } from './pages/Main'
@@ -12,13 +11,11 @@ import { Status } from './pages/Status'
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SkeletonTheme baseColor="#0D0D0D" highlightColor="#525252">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/status" element={<Status />} />
-        </Routes>
-      </BrowserRouter>
-    </SkeletonTheme>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/status" element={<Status />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
