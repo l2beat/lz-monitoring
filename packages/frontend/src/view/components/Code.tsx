666a47ce41d5406ebd9fcc29a0592e35f086671a
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-11-15 22:00:15 +0100

    style: sort css classes

--- packages/frontend/src/view/components/Code.tsx
+++ packages/frontend/src/view/components/Code.tsx
@@ -9,7 +9,7 @@ export function Code({ children, className }: Props) {
   return (
     <pre
       className={cx(
-        'scrollbar-track-gray-300 scrollbar-h-1.5 overflow-auto rounded bg-gray-400 p-3 font-mono text-xs text-gray-500 scrollbar scrollbar-thumb-[#eef36a]',
+        'scrollbar-h-1.5 overflow-auto rounded bg-gray-400 p-3 font-mono text-xs text-gray-500 scrollbar scrollbar-track-gray-300 scrollbar-thumb-[#eef36a]',
         className,
       )}
     >
