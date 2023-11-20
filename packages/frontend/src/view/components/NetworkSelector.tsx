7f122e0ba9766d9592969fb6bab8d67b253c4775
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-11-20 18:19:54 +0100

    refac: NetworkSelector width contraints

--- packages/frontend/src/view/components/NetworkSelector.tsx
+++ packages/frontend/src/view/components/NetworkSelector.tsx
@@ -35,20 +35,24 @@ export function NetworkDropdownSelector(props: Props) {
     setChainId(props.chainsToDisplay[0])
   }
 
-  const pills = props.chainsToDisplay.map((chainToDisplay, i) => (
-    <PillSelector
-      key={i}
-      icon={getIconForChain(chainToDisplay)}
-      label={getPrettyChainName(chainToDisplay)}
-      isActive={chainToDisplay === optimisticSelect}
-      onClick={() => setChainId(chainToDisplay)}
-    />
-  ))
+  const pills = props.chainsToDisplay
+    .map((chainToDisplay, i) => ({
+      key: i,
+      icon: getIconForChain(chainToDisplay),
+      label: getPrettyChainName(chainToDisplay),
+      isActive: chainToDisplay === optimisticSelect,
+      onClick: () => setChainId(chainToDisplay),
+    }))
+    .sort((a, b) => a.label.localeCompare(b.label))
+    .map((props) => <PillSelector {...props} />)
 
   return (
-    <section className="flex justify-center bg-gray-500 px-6 py-8">
-      <div className="scrollbar-h-1.5 flex items-stretch gap-3 overflow-auto overflow-x-auto py-2 scrollbar scrollbar-track-gray-50 scrollbar-thumb-[#eef36a]">
-        {pills}
+    <section className="bg-gray-500 px-6 py-8 md:flex md:justify-center">
+      <div className="md:max-w-[1650px]">
+        <span className="text-xs text-gray-15">Select network</span>
+        <div className="scrollbar-h-1.5 flex items-stretch gap-3 overflow-x-auto py-2 scrollbar scrollbar-track-gray-50 scrollbar-thumb-[#eef36a]">
+          {pills}
+        </div>
       </div>
     </section>
   )
