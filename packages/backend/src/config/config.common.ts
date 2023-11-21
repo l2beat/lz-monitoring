c730b0d41d686ed9dff66193ee14a11f5084f005
Michał Podsiadły <67391475+sdlyy@users.noreply.github.com>
2023-11-21 12:49:57 +0100

    Migrate avalanche to routescan explorer (#64)

--- packages/backend/src/config/config.common.ts
+++ packages/backend/src/config/config.common.ts
@@ -166,13 +166,18 @@ export function getCommonDiscoveryConfig(env: Env): Config['discovery'] {
          */
         chainNamePrefix: 'AVALANCHE',
         startBlock: 12126063,
-        blockExplorerPrefix: 'SNOWTRACE',
-        blockExplorerApiUrl: 'https://api.snowtrace.io/api',
+        blockExplorerPrefix: 'AVALANCHE_ROUTESCAN',
+        blockExplorerApiUrl:
+          'https://api.routescan.io/v2/network/mainnet/evm/43114/etherscan/api',
         blockExplorerMinTimestamp: new Date('2020-09-23T11:02:00Z'),
         discoveryConfig: avalancheDiscoveryConfig,
         eventsToWatchConfig: avalancheEventsToWatch,
         changelogWhitelist: avalancheChangelogWhitelist,
         multicallConfig: multicallConfig.avalanche,
+        unsupportedEtherscanMethods: {
+          getContractCreation: true,
+        },
+        explorerApiKeyRequired: false,
       }),
       linea: createConfig({
         /**
@@ -238,6 +243,7 @@ function configFromTemplate(env: Env) {
     changelogWhitelist,
     multicallConfig,
     unsupportedEtherscanMethods,
+    explorerApiKeyRequired = true,
   }: {
     /**
      * The prefix of the environment variables that configure the chain.
@@ -288,6 +294,11 @@ function configFromTemplate(env: Env) {
      * Etherscan unsupported methods
      */
     unsupportedEtherscanMethods?: EtherscanUnsupportedMethods
+
+    /**
+     * Whether the block explorer API key is required
+     */
+    explorerApiKeyRequired?: boolean
   }): DiscoverySubmoduleConfig {
     const isEnabled = env.boolean(`${chainNamePrefix}_DISCOVERY_ENABLED`, false)
     const isVisible = env.boolean(`${chainNamePrefix}_VISIBLE`, false)
@@ -316,7 +327,9 @@ function configFromTemplate(env: Env) {
           `${chainNamePrefix}_EVENT_INDEXER_AMT_BATCHES`,
         ),
         blockExplorerApiUrl,
-        blockExplorerApiKey: env.string(`${blockExplorerPrefix}_API_KEY`),
+        blockExplorerApiKey: explorerApiKeyRequired
+          ? env.string(`${blockExplorerPrefix}_API_KEY`)
+          : '',
         blockExplorerMinTimestamp: new UnixTime(
           env.integer(
             `${blockExplorerPrefix}_MIN_TIMESTAMP`,
