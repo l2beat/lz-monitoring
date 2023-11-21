ebb5cf5ad7a472a7bd560b44aa4ac24c8bb063a4
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-11-21 22:03:50 +0100

    feat: Row label padding

--- packages/frontend/src/view/components/Row.tsx
+++ packages/frontend/src/view/components/Row.tsx
@@ -7,10 +7,10 @@ export function Row({
 }) {
   return (
     <div className="flex flex-col items-center justify-center py-0.5 md:flex-row">
-      <div className="py-2 text-sm font-medium leading-5 text-gray-15 md:w-1/5 md:p-0">
+      <div className="py-2 pr-2 text-sm font-medium leading-5 text-gray-15 md:w-1/5 md:py-0">
         {label}
       </div>
-      <div className="w-full pb-2 text-center font-mono text-3xs md:w-4/5 md:p-0 md:py-4 md:text-left md:text-sm">
+      <div className="w-full pb-2 text-center font-mono text-3xs md:w-4/5 md:px-0 md:py-4 md:text-left md:text-sm">
         {value}
       </div>
     </div>
