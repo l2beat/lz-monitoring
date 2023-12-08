fbddf9bfc302ee76b5ade4083b291b8ed0322dad
Michał Podsiadły <67391475+sdlyy@users.noreply.github.com>
2023-12-08 12:43:28 +0100

    Fix decoded data regression (#93)

--- packages/frontend/src/view/components/safe/SafeMultisigTransaction.tsx
+++ packages/frontend/src/view/components/safe/SafeMultisigTransaction.tsx
@@ -42,6 +42,7 @@ export function SafeMultisigTransaction({
   const decodedProperties = getDecodedProperties(transaction)
 
   const method = decodedProperties?.method ?? 'Could not be decoded ⚠️'
+  const signature = decodedProperties?.signature ?? 'Could not be decoded ⚠️'
   const callWithParams =
     decodedProperties?.callWithParams ?? 'Could not be decoded ⚠️'
   const params = decodedProperties?.params ?? []
@@ -100,7 +101,10 @@ export function SafeMultisigTransaction({
             param="Target"
             value={<BlockchainAddress address={EthereumAddress(target)} />}
           />
-          <TransactionProperty param="Method" value={<Code>{method}</Code>} />
+          <TransactionProperty
+            param="Function signature"
+            value={<Code>{signature}</Code>}
+          />
           <TransactionProperty
             param="Call with params"
             value={<Code>{callWithParams}</Code>}
@@ -115,7 +119,9 @@ export function SafeMultisigTransaction({
               value={
                 <Code>
                   {params.map((inlineSummary) => (
-                    <span className="leading-5">{inlineSummary}</span>
+                    <span className="leading-5">
+                      {paramToSummary(inlineSummary).concat('\n')}
+                    </span>
                   ))}
                 </Code>
               }
@@ -141,7 +147,7 @@ function getDecodedProperties(tx: SafeMultisigTransaction) {
       method: decodedCall.method,
       signature: decodedCall.signature,
       callWithParams: decodedCall.functionCall,
-      params: decodedCall.params.map(paramToSummary),
+      params: decodedCall.params,
     }
   }
 
