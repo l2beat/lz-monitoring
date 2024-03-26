497c6a572479fe826ba29833b789ced498c874c3
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-03-26 14:12:36 +0100

    feat: add context to frontend addresses

--- packages/frontend/src/pages/Applications.tsx
+++ packages/frontend/src/pages/Applications.tsx
@@ -1,10 +1,13 @@
 import { ChainId } from '@lz/libs'
+import { SkeletonTheme } from 'react-loading-skeleton'
 
 import { config } from '../config'
+import { AddressInfoContext } from '../hooks/addressInfoContext'
 import { useTrackingApi } from '../hooks/useTrackingApi'
 import { Layout } from '../view/components/Layout'
 import { Navbar } from '../view/components/Navbar'
-import { OAppTable } from '../view/components/oapp/OAppTable'
+import { OAppTable, OAppTableSkeleton } from '../view/components/oapp/OAppTable'
+import { Warning } from '../view/components/Warning'
 
 export function Applications() {
   const chainsToDisplay = [ChainId.ETHEREUM] as [ChainId, ...ChainId[]]
@@ -14,21 +17,41 @@ export function Applications() {
     apiUrl: config.apiUrl,
   })
 
-  if (!oApps || isLoading) {
-    return 'Loading'
+  if (isLoading) {
+    return (
+      <Page>
+        <SkeletonTheme baseColor="#27272A" highlightColor="#525252">
+          <OAppTableSkeleton />
+        </SkeletonTheme>
+      </Page>
+    )
   }
 
-  if (isError) {
-    return 'Error'
+  if (!oApps || isError) {
+    return (
+      <Page>
+        <Warning
+          title="Could not load applications"
+          subtitle="Applications API might be unreachable or no applications were discovered. Please try again later."
+        />
+      </Page>
+    )
   }
 
   return (
+    <Page>
+      <AddressInfoContext.Provider value={oApps.data.addressInfo}>
+        <OAppTable oAppsList={oApps.data} />
+      </AddressInfoContext.Provider>
+    </Page>
+  )
+}
+
+function Page({ children }: { children: React.ReactNode }) {
+  return (
     <>
       <Navbar />
-
-      <Layout>
-        <OAppTable oAppsList={oApps.data} />
-      </Layout>
+      <Layout>{children}</Layout>
     </>
   )
 }
--- packages/frontend/src/view/components/oapp/OAppTable.tsx
+++ packages/frontend/src/view/components/oapp/OAppTable.tsx
@@ -1,8 +1,35 @@
 import { ChainId, OAppsResponse } from '@lz/libs'
+import Skeleton from 'react-loading-skeleton'
 
 import { OAppRow } from './OAppRow'
 
 export function OAppTable({ oAppsList }: { oAppsList: OAppsResponse }) {
+  return (
+    <OAppTableWrapper>
+      {oAppsList.oApps.map((oApp) => (
+        <OAppRow
+          key={oApp.name}
+          oApp={oApp}
+          defaults={oAppsList.defaultConfigurations}
+          sourceChain={ChainId.ETHEREUM}
+        />
+      ))}
+    </OAppTableWrapper>
+  )
+}
+
+export function OAppTableSkeleton() {
+  return (
+    <OAppTableWrapper>
+      <Skeleton className="mt-3" />
+      <Skeleton />
+      <Skeleton />
+      <Skeleton />
+    </OAppTableWrapper>
+  )
+}
+
+function OAppTableWrapper({ children }: { children: React.ReactNode[] }) {
   return (
     <div className="mt-10 overflow-x-auto rounded bg-gray-900 px-7 py-5">
       <div className="col-span-5 grid min-w-[800px] grid-cols-applications rounded bg-gray-600 py-3 text-left text-[13px] font-semibold text-gray-50">
@@ -13,14 +40,8 @@ export function OAppTable({ oAppsList }: { oAppsList: OAppsResponse }) {
         <div>CONFIG</div>
         <div />
       </div>
-      {oAppsList.oApps.map((oApp) => (
-        <OAppRow
-          key={oApp.name}
-          oApp={oApp}
-          defaults={oAppsList.defaultConfigurations}
-          sourceChain={ChainId.ETHEREUM}
-        />
-      ))}
+
+      {children}
     </div>
   )
 }
